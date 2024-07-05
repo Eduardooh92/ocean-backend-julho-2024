a36c4aa99ee8966cbc4591b31df9800009cdc2b6
Eduardo Loureiro <142263683+Eduardooh92@users.noreply.github.com>
2024-07-05 12:03:00 +0000

    Conexão com o Banco de dados

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express')
+const { MongoClient } = require('mongodb')
 const app = express()
+
+const dbUrl = 'mongodb+srv://admin:hX1f3KxkJ0iRye82@cluster0.nqof4rz.mongodb.net/'
+const dbName = 'ocean-jornada-backend'
+
+const client = new MongoClient(dbUrl)
+
+async function main() {
+console.log('Conectando ao banco de dados...');
+await client.connect()
+console.log('Banco de dados conectado com sucesso');
 
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -18,4 +29,19 @@ app.get('/item', function (req, res) {
   res.send(lista)
 })
 
+//Sinalizando para o Express que vamos usar JSOn no Body
+app.use(express.json())
+
+// Create - [POST] /item
+app.post('/item', function(req, res) {
+  console.log(req.body);
+  const item = req.body
+  res.send('Create')
+})
+
+
+
 app.listen(3000)
+}
+
+main()
