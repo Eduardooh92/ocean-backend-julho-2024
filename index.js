1d7e28ef8d038d9ed87dbddc8c53051504248113
Eduardo Loureiro <142263683+Eduardooh92@users.noreply.github.com>
2024-07-05 12:21:57 +0000

    Banco de dados integrado com Read All

--- index.js
+++ index.js
@@ -8,40 +8,48 @@ const dbName = 'ocean-jornada-backend'
 const client = new MongoClient(dbUrl)
 
 async function main() {
-console.log('Conectando ao banco de dados...');
-await client.connect()
-console.log('Banco de dados conectado com sucesso');
+  console.log('Conectando ao banco de dados...');
+  await client.connect()
+  console.log('Banco de dados conectado com sucesso');
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
 
-app.get('/oi', function (req, res) {
-  res.send('Oi mundo!')
-})
-
-
-//Lista de personagens
-const lista = ['Rick Sanchez', 'Morty Smith', 'Summer  Smith']
-
-//Read All - [GET] /item
-app.get('/item', function (req, res) {
-  res.send(lista)
-})
-
-//Sinalizando para o Express que vamos usar JSOn no Body
-app.use(express.json())
-
-// Create - [POST] /item
-app.post('/item', function(req, res) {
-  console.log(req.body);
-  const item = req.body
-  res.send('Create')
-})
+  app.get('/oi', function (req, res) {
+    res.send('Oi mundo!')
+  })
 
 
+  //Lista de personagens
+  const lista = ['Rick Sanchez', 'Morty Smith', 'Summer  Smith']
 
-app.listen(3000)
+  const db = client.db(dbName)
+  const collection = db.collection('item')
+
+  //Read All - [GET] /item
+  app.get('/item', async function (req, res) {
+    //Obter todos os documentos da collection
+    const documentos = await collection.find().toArray()
+    
+    //Pegamos os documentos e enviamos como resposta HTTP
+    res.send(documentos)
+  })
+
+
+  //Sinalizando para o Express que vamos usar JSOn no Body
+  app.use(express.json())
+
+  // Create - [POST] /item
+  app.post('/item', function (req, res) {
+    console.log(req.body);
+    const item = req.body
+    res.send('Create')
+  })
+
+
+
+  app.listen(3000)
 }
 
 main()
